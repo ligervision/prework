4b99d6c82f093b21ac0273db45734a5bbbbf9d6b
ligervision <104537623+ligervision@users.noreply.github.com>
2022-05-02 09:29:37 -0500

    Add files via upload

---
+++ prework_python1.py
@@ -0,0 +1,57 @@
+# Question 1 - Write a function to print "hello_USERNAME!" 
+# USERNAME is the input of the function. 
+# The first line of the code has been defined as below. 
+
+
+def hello_name(user_name):
+    print ("hello_" + user_name)
+
+user_name = "USERNAME!"
+
+
+
+# Question 2 - Write a python function, first_odds that prints the 
+# odd numbers from 1-100 and returns nothing 
+
+def first_odds(): 
+    if int % 2 == 0:
+
+    print(list(range(0:100)))
+
+
+
+
+# Question 3 - Please write a Python function, max_num_in_list to 
+# return the max number of a given list. The first line of the 
+# code has been defined as below.
+
+def max_num_in_list(a_list):
+    for num in a_list:
+        if num 
+
+
+
+# Question 4 - Write a function to return if the given year is a 
+# leap year. A leap year is divisible by 4, but not divisible by 
+# 100 unless it is also divisible by 400. The return should be 
+# boolean Type (true/false).
+
+def is_leap_year(a_year):
+    for range(4:3000)
+    if a_year // 4 == 0 or a_year // 400 == 0:
+        print("Yes this is a Leap Year")
+    else:
+        print("No, this is not a Leap Year")
+
+
+
+# Question 5 - Write a function to check to see if all numbers in 
+# the list are consecutive numbers. For example, [2,3,4,5,6,7] are 
+# consecutive numbers, but [1,2,4,5] are not consecutive numbers. 
+# The return should be boolean Type.
+
+def is_consecutive(a_list):
+    
+
+
+
