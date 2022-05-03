e14a2d2bca91c94fc2e4b783521a44fee22c1023
ligervision <104537623+ligervision@users.noreply.github.com>
2022-05-02 21:20:37 -0500

    corrected during lecture

--- prework_python1.py
+++ prework_python1.py
@@ -1,57 +1,86 @@
-# Question 1 - Write a function to print "hello_USERNAME!" 
-# USERNAME is the input of the function. 
-# The first line of the code has been defined as below. 
-
+# Question 1
 
 def hello_name(user_name):
-    print ("hello_" + user_name)
-
-user_name = "USERNAME!"
-
-
-
-# Question 2 - Write a python function, first_odds that prints the 
-# odd numbers from 1-100 and returns nothing 
-
-def first_odds(): 
-    if int % 2 == 0:
-
-    print(list(range(0:100)))
+    print("Hello" + user_name.upper() + "!")
+    
+hello_name('Carlos')
 
 
 
+# Question 2
 
-# Question 3 - Please write a Python function, max_num_in_list to 
-# return the max number of a given list. The first line of the 
-# code has been defined as below.
+def odd_numbers():
+    for i in range(1,101,2):
+        print(i)
+        
+# odd_numbers()
+        
+def odd_numbers2():
+    numbers = list(range(0, 101))
+    for number in numbers:
+    	if number % 2 != 0:
+        	print(number)
+        
+odd_numbers2()
+
+
+
+# Question 3
 
 def max_num_in_list(a_list):
-    for num in a_list:
-        if num 
+    max_num = max(a_list)
+    return max_num
+
+test = max_num_in_list([2,3,5,8,9])
+print(max_num_in_list([2,3,5,8,9]))
 
 
 
-# Question 4 - Write a function to return if the given year is a 
-# leap year. A leap year is divisible by 4, but not divisible by 
-# 100 unless it is also divisible by 400. The return should be 
-# boolean Type (true/false).
+# Question 4
 
 def is_leap_year(a_year):
-    for range(4:3000)
-    if a_year // 4 == 0 or a_year // 400 == 0:
-        print("Yes this is a Leap Year")
+    if a_year % 4 == 0 and a_year % 100 != 0:
+        print(f'{a_year} is a leap year')
+    elif a_year % 400 == 0:
+        print(f'{a_year} is a leap year')
     else:
-        print("No, this is not a Leap Year")
+        a_year = False
+        print(f'{a_year}')
+
+
+# Question 4 alt. solution
+
+def is_leap(a_year):
+    if a_year % 4 == 0 and (a_year % 400 == 0 or a_year % 100 != 0):
+        print(True)
+    else:
+        print(False)
+
+is_leap_year(2019)
+is_leap(2019)
 
 
 
-# Question 5 - Write a function to check to see if all numbers in 
-# the list are consecutive numbers. For example, [2,3,4,5,6,7] are 
-# consecutive numbers, but [1,2,4,5] are not consecutive numbers. 
-# The return should be boolean Type.
+# Question 5
 
 def is_consecutive(a_list):
+    i = 0
+    status = True
+    while i < len(a_list) - 1:
+       if a_list[i] + 1 == a_list[i + 1]:
+           i += 1
+       else:
+            status = False
+            break
+    print(status)
+
+
+
     
+
+
+
+
 
 
 
